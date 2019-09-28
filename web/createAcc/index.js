ac162d48ec1c305d69706169986299e8c040ff07
Alex Collantes <acoll094@fiu.edu>
2019-09-28 14:58:36 -0400

    Saving user input

---
+++ web/createAcc/index.js
@@ -0,0 +1,8 @@
+function createAccount() {
+    var email = document.getElementById("userEmail").value;
+    alert(email);
+    var username = document.getElementById("userName").value;
+    alert(username);
+    var password = document.getElementById("userPass").value;
+    alert(password);
+}
