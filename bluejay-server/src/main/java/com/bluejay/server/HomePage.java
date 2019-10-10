fe3baa59ab651e8aadf19da001a9dc22ef08eb1d
David Ramirez <drami102@fiu.edu>
2019-10-10 16:40:32 -0400

    Moving over to java based web server

---
+++ bluejay-server/src/main/java/com/bluejay/server/HomePage.java
@@ -0,0 +1,28 @@
+package com.bluejay.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/test")
+public class HomePage extends HttpServlet
+{
+    /**
+     * Here just so I can ignore the stupid warning.
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException
+    {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<p>Hello World!</p>");
+    }
+
+
+}
