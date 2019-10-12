e732fe471af8369d0ab86e0a03780f854495b614
David Ramirez <drami102@fiu.edu>
2019-10-12 11:24:42 -0400

    Moved sql connection

--- src/main/java/com/bluejay/server/db/SQLDatabaseConnection.java
+++ src/main/java/com/bluejay/server/db/SQLDatabaseConnection.java
@@ -1,15 +1,21 @@
+package com.bluejay.server.db;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class SQLDatabaseConnection {
 
     private Connection dbconnect;
 
     public SQLDatabaseConnection()
+    throws SQLException, NamingException
     {
         Context initCtx = new InitialContext();
         Context envCtx = (Context) initCtx.lookup("java:comp/env");
@@ -17,6 +23,7 @@ public class SQLDatabaseConnection {
         dbconnect = ds.getConnection();
     }
     public String getPost(int id)
+    throws SQLException
     {
         PreparedStatement st = dbconnect.prepareStatement("SELECT * FROM ");
         st.setInt(1,id);
