0ac2ddbb717a4067dfa34f44a650fbe58b537004
David Ramirez <drami102@fiu.edu>
2019-10-19 10:46:05 -0400

    Fixed class name

--- src/main/java/com/bluejay/server/pages/CreateAccount.java
+++ src/main/java/com/bluejay/server/pages/CreateAccount.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.bluejay.server.pages.Page;
 
 @WebServlet("/test3")
-public class CreateAcc
+public class CreateAccount
 extends Page
 {
     
