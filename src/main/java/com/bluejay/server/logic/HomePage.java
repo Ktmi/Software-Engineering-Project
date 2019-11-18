9d680a96415130ae6d6e36dba85c26b4343e4bb9
David Ramirez <drami102@fiu.edu>
2019-11-18 15:08:33 -0500

    Updating injections

--- src/main/java/com/bluejay/server/logic/HomePage.java
+++ src/main/java/com/bluejay/server/logic/HomePage.java
@@ -6,20 +6,29 @@ import java.util.List;
 import java.util.ArrayList;
 
 import javax.ws.rs.Path;
+import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 
 
 import org.glassfish.jersey.server.mvc.Template;
+
+import com.bluejay.server.db.DatabaseFacade;
+
 import org.glassfish.jersey.server.mvc.ErrorTemplate;
 
 @Path("/")
 @Produces({"text/html"})
 public class HomePage {
 
+	@Resource(name = "jbdc/bluejay-db")
+	private DatabaseFacade databaseFacade;
+	
+	
     @GET
     @Template(name = "/index.ftl")
     @ErrorTemplate(name = "/error.ftl")
+    @Produces("text/html")
     public Map<String,Object> presentHomepage()
     {
         Map<String,Object> a = new HashMap<String,Object>();
