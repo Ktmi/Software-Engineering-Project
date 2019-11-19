f4b1ae68fa1a43ae9392d814c9a0b2043f67e8f6
David Ramirez <drami102@fiu.edu>
2019-11-19 10:54:03 -0500

    Began work on implementing authorizationfilter

--- src/main/java/com/bluejay/server/logic/AuthorizationFilter.java
+++ src/main/java/com/bluejay/server/logic/AuthorizationFilter.java
@@ -1,20 +1,45 @@
 package com.bluejay.server.logic;
 
 import java.io.IOException;
+import java.security.Key;
+import java.util.logging.Logger;
 
+import javax.crypto.KeyGenerator;
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+
+import io.jsonwebtoken.Jwts;
 
 
 public class AuthorizationFilter
 implements ContainerRequestFilter
 {
-
+	
+	@Inject
+	private Logger logger;
+	
+	@Inject
+	private KeyGenerator keyGenerator;
+	
     @Override
     public void filter(ContainerRequestContext requestContext)
     throws IOException
     {
         String authHeaderVal = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+        String token = authHeaderVal.substring("Bearer".length()).trim();
+        try
+        {
+        	Key key = keyGenerator.generateKey();
+        	Jwts.parser().setSigningKey(key).parseClaimsJwt(token);
+        	logger.info("#### valid token : " + token);
+        }
+        catch (Exception e)
+        {
+        	logger.severe("#### invalid token : " + token);
+        	requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+        }
     }
 }
