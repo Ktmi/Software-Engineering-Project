febc84ecd55ea671fb785e6a2ad02a234f2fef61
David Ramirez <drami102@fiu.edu>
2019-11-21 14:58:08 -0500

    Commented out buggy code for the moment

--- src/main/java/com/bluejay/server/logic/HomePage.java
+++ src/main/java/com/bluejay/server/logic/HomePage.java
@@ -1,6 +1,5 @@
 package com.bluejay.server.logic;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,13 +9,11 @@ import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.server.mvc.ErrorTemplate;
 import org.glassfish.jersey.server.mvc.Template;
 
-import com.bluejay.server.common.Post;
 import com.bluejay.server.db.DatabaseFacade;
 
 @Path("/")
@@ -39,11 +36,14 @@ public class HomePage {
 		return a;
 	}
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Post> search(@QueryParam("query") String query, @QueryParam("orderBy") List<String> orderBy,
-			@QueryParam("from") int from, @QueryParam("to") int to) throws SQLException {
-		return databaseFacade.search(query, orderBy, from, to);
-	}
-
+	/*
+	 * @GET
+	 * 
+	 * @Produces(MediaType.APPLICATION_JSON) public List<Post>
+	 * search(@QueryParam("query") String query, @QueryParam("orderBy") List<String>
+	 * orderBy,
+	 * 
+	 * @QueryParam("from") int from, @QueryParam("to") int to) throws SQLException {
+	 * return databaseFacade.search(query, orderBy, from, to); }
+	 */
 }
